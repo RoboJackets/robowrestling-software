6e69508d52587ceaab236f3cd0d4b2433b0bed33
chang.andrew.23@gmail.com <andrew_chang@lawn-143-215-50-254.lawn.gatech.edu>
2017-10-05 22:03:52 -0400

    formatted code to match new strategy

---
+++ v2/pushiv_05oct/pushiv_05oct.ino
@@ -0,0 +1,359 @@
+/*
+SharpDistSensorBasic.ino
+Source: https://github.com/DrGFreeman/SharpDistSensor
+MIT License
+Copyright (c) 2017 Julien de la Bruere-Terreault <drgfreeman@tuta.io>
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+This example shows how to use the SharpDistSensor library to continuously
+read the sensor and display the analog value and the corrseponding distance
+in mm.
+The library default values corresponding to the Sharp GP2Y0A60SZLF 5V sensor
+are used.
+See the library README for how to use pre-defined sensor models or custom
+fit functions.
+*/
+
+#include <SharpDistSensor.h>
+
+// Analog pin to which the sensor is connected
+const byte sensorPin = A0; //right IR sensor
+const byte sensorPin2 = A1; //left IR sensor
+const byte backleft = A6; //back-left line sensor
+const byte backright = A7; //back-right line sensor
+const byte frontright = A2; //front-left line sensor
+const byte frontleft = A3; //front-right line sensor
+const byte sensorPin4 = A4; //A4: left peripheral sensor
+const byte sensorPin3 = A5;//A5: right peripheral sensor
+
+// Window size of the median filter (odd number, 1 = no filtering)
+const byte mediumFilterWindowSize = 5;
+unsigned int rightDist; //distance from right sensor
+unsigned int leftDist; //distance from left sensor
+unsigned int perRightDist; //distance from peripheral right sensor
+unsigned int perLeftDist; //distance from peripheral left sensor
+
+// Create an object instance of the SharpDistSensor class
+SharpDistSensor sensor(sensorPin, mediumFilterWindowSize); //right sensor
+SharpDistSensor sensor2(sensorPin2, mediumFilterWindowSize); //left sensor
+SharpDistSensor sideright(sensorPin3, mediumFilterWindowSize); //right peripheral sensor
+SharpDistSensor sideleft(sensorPin4, mediumFilterWindowSize); //left peripheral sensor
+int STBY = 10; //standby
+
+//Right Motor (? I'm not too sure about this)
+int PWMA = 3; //Speed control
+int AIN1 = 9; //Direction
+int AIN2 = 8; //Direction
+
+//Left Motor (? also not too sure about this)
+int PWMB = 5; //Speed control
+int BIN1 = 11; //Direction
+int BIN2 = 12; //Direction
+
+char past; //what direction the robot was moving previously
+int count; //counter for how long before the rebot gives up searching in one direction
+int threshold; //threshold for line sensors
+int curIR; // current time
+int prevIR; 
+int curLine; 
+int prevLine; 
+short iter;
+int maxS;
+int minS;
+int rangeThresh;
+boolean start;
+boolean lineFlag;
+boolean pivotFlag;
+
+int near = 200;
+int far = 500;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(STBY, OUTPUT);
+
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  
+  pinMode(PWMB, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+
+  //initialising values
+  past = 'f'; //set default move to forward
+  start = true; //set startup flag
+  count = 0;
+  iter = 0;
+  threshold = analogRead(backleft) - 200;
+  rightDist = sensor.getDist();
+  leftDist = sensor2.getDist();
+  perRightDist = sideright.getDist();
+  perLeftDist = sideleft.getDist();
+  prevLine = millis();
+  prevIR = prevLine;
+  lineFlag = false;
+  pivotFlag = false;
+  setSpeed(128, 20);
+}
+
+void loop() {
+  curLine = millis();
+  curIR = curLine;
+  
+  //print out values from sensors and motors to the console
+  Serial.print(past);
+  Serial.print(", ");
+  Serial.print(rightDist);
+  Serial.print(", ");
+  Serial.print(leftDist);
+  Serial.print(", ");
+  Serial.print(perRightDist);
+  Serial.print(", ");
+  Serial.print(perLeftDist);
+  Serial.print(", ");
+  Serial.print(analogRead(backleft));
+  Serial.print(", ");
+  Serial.print(analogRead(backright));
+  Serial.print(", ");
+  Serial.print(analogRead(frontleft));
+  Serial.print(", ");
+  Serial.println(analogRead(frontright));
+  Serial.print(", ");
+  Serial.println(curLine - prevLine);
+
+  if(start) {
+    startUp();
+  }
+    
+  if ((curLine - prevLine) >= 5) {
+    prevLine = curLine;
+    if (analogRead(backleft) < threshold || analogRead(backright) < threshold) { //first read from line sensors before doing anything else
+      //if line detected by back line sensors, turn left a bit
+      forward();
+      past = 'f'; //set default move to forward
+      delay(50); //wait 50ms for the IR sensors to take new reading
+      lineFlag = true;
+    } else if (analogRead(frontleft) < threshold && past == 'l') {
+      backRight();
+      delay(25);
+      leftInPlace();
+      past = 'f';
+      delay(50);
+      lineFlag = true;
+    } else if (analogRead(frontleft) < threshold && past == 'r' || analogRead(frontleft) < threshold && past == 'f') {
+      backRight();
+      delay(25);
+      rightInPlace();
+      past = 'f';
+      delay(50);
+      lineFlag = true;
+    } else if (analogRead(frontright) < threshold && past == 'l') {
+      backLeft();
+      delay(25);
+      rightInPlace();
+      past = 'f';
+      delay(50);
+      lineFlag = true;
+    } else if (analogRead(frontright) < threshold && past == 'r' || analogRead(frontright) < threshold && past == 'f') {
+      backLeft();
+      delay(25);
+      leftInPlace();
+      past = 'f';
+      delay(50);
+      lineFlag = true;
+    } else if (analogRead(frontright) < threshold && analogRead(frontleft) < threshold) {
+      back();
+      delay(25);
+      rightInPlace();
+      past = 'f';
+      delay(100);
+      lineFlag = true;
+    } else if ((curIR - prevIR) >= 25) {
+      prevIR = curIR;
+      // Get distance from sensors
+      rightDist = sensor.getDist();
+      leftDist = sensor2.getDist();
+      perRightDist = sideright.getDist();
+      perLeftDist = sideleft.getDist();
+
+      //attack code with sensors
+
+      if (!pivotFlag) {
+        //other attack cases
+        
+        if (perLeftDist <= near) {
+          //if there is an object within nearmm of the left peripheral sensor, move left
+          //turn left in place
+          leftInPlace();
+          past = 'l'; //set past movement to left
+          count = 0; //reset abandon search count because the robot has found an object
+        } else if (perRightDist <= near) {
+          //if there is an object within nearmm of the right peripheral sensor, move right
+          //turn right in place
+          rightInPlace();
+          past = 'r'; //set past movement to left
+          count = 0; //reset abandon search count because the robot has found an object
+        } else if (perRightDist <= near && perLeftDist <= near) {
+          //if there is an object within nearmm of both peripheral sensors, move forward
+          forward();
+          past = 'f'; //set past movement to left
+          count = 0; //reset abandon search count because the robot has found an object
+        } 
+        
+        if (!lineFlag) {
+          if ((abs(rightDist - leftDist) <= 150) 
+                || ((past == 'f' || count >= 100) && (rightDist > far && leftDist > far && perRightDist > far && perLeftDist > far))) {
+            //if (robot moved forward in the past OR it's been searching for 100 iterations) AND (there isn't an object within far of any IR sensor))
+            //OR there is an object within near of both sensors 
+            //OR the difference between the two distances measured by the IR sensors is within 150mm of each other
+            //move forward
+            forward();
+            past = 'f'; //set past movement to forward
+            count = 0; //reset abandon search count
+          } else if ((leftDist <= far || perLeftDist <= far) || leftDist - rightDist < -150) {
+            //if there is an object beyond farmm of the right sensors (i.e. there is something closer in the left sensors), move left
+            left();
+            past = 'l'; //set past movement to left
+            count = 0; //reset abandon search count because the robot has found an object
+          } else if ((rightDist <= far || perRightDist <= far) || leftDist - rightDist > 150) {
+            //if there is an object beyond farmm of the left sensors (i.e. there is something closer in the right sensors), move right
+            right();
+            past = 'r'; //set past movement to right
+            count = 0; //reset abandon search count because the robot has found an object
+          } else if (past == 'l') {
+            //if robot has been moving left in the past, move left more
+            left();
+            past = 'l'; //set past movement to left
+            count++; //increment abandon search count
+          } else if (past == 'r') {
+            //if robot has been moving right in the past, move right
+            right();
+            past = 'r'; //set past movement to right
+            count++; //increment abandon search count
+          }
+        }
+              
+          
+//        } else if (past == 'l' && (rightDist > far && leftDist > far && perRightDist > far && perLeftDist > far)) {
+//          //if robot has been moving left in the past, move left more
+//          left();
+//          past = 'l'; //set past movement to left
+//          count++; //increment abandon search count
+//        } else if (past == 'r' && (rightDist > far && leftDist > far && perRightDist > far && perLeftDist > far)) {
+//          //if robot has been moving right in the past, move right
+//          right();
+//          past = 'r'; //set past movement to right
+//          count++; //increment abandon search count
+//        }
+      }
+    }
+  }
+}
+
+void move(int motor, int speed, int direction){
+  //Move specific motor at speed and direction
+  //motor: 1 for right, 2 for left
+  //speed: 0 is off, and 255 is full speed
+  //direction: 0 clockwise, 1 counter-clockwise
+  
+  digitalWrite(STBY, HIGH); //disable standby
+  
+  boolean inPin1 = LOW;
+  boolean inPin2 = HIGH;
+  
+  if(direction == 1){
+    inPin1 = HIGH;
+    inPin2 = LOW;
+  }
+  
+  if(motor == 1){
+    digitalWrite(AIN1, inPin1);
+    digitalWrite(AIN2, inPin2);
+    analogWrite(PWMA, speed);
+  } else {
+    digitalWrite(BIN1, inPin1);
+    digitalWrite(BIN2, inPin2);
+    analogWrite(PWMB, speed);
+  }
+}
+
+void stop(){
+  //enable standby
+  digitalWrite(STBY, LOW);
+}
+
+void forward() {
+  move(1, maxS, 1); //right, full speed, counter clockwise
+  move(2, maxS, 0); //left, full speed, clockwise
+}
+
+void attack() {
+  move(1, 255, 1); //right, full speed, counter clockwise
+  move(2, 255, 0); //left, full speed, clockwise
+}
+
+void left() {
+  move(1, minS, 1); //right, min speed, counter clockwise
+  move(2, maxS, 0); //left, full speed, clockwise
+}
+
+void right() {
+  move(1, maxS, 1); //right, full speed, counter clockwise
+  move(2, minS, 0); //left, full speed, clockwise
+}
+
+void leftInPlace() {
+  move(1, maxS, 0); //right, full speed, clockwise
+  move(2, maxS, 0); //left, full speed, clockwise
+}
+
+void rightInPlace() {
+  move(1, maxS, 1); //right, full speed, counter clockwise
+  move(2, maxS, 1); //left, full speed, counter clockwise
+}
+
+void backLeft() {
+  move(1, minS, 0); //right, min speed, counter clockwise
+  move(2, maxS, 1); //left, full speed, clockwise
+}
+
+void backRight() {
+  move(1, maxS, 0); //right, full speed, counter clockwise
+  move(2, minS, 1); //left, full speed, clockwise
+}
+
+void back() {
+  move(1, maxS, 0); //right, full speed, counter clockwise
+  move(2, maxS, 1); //left, full speed, clockwise
+}
+
+void setSpeed(int ma, int mi) {
+  maxS = ma;
+  minS = mi;
+}
+
+void startUp() {
+  //startup code here
+}
+
+void pivot() {
+  //pivot code here
+}
+
