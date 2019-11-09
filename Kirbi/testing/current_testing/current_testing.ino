7dc7e83e9c46fd1ee1cf3e97a666ca26e3ac6133
Logan Schick <schicklogan@gmail.com>
2019-11-08 21:25:26 -0500

    Added current_testing.ino

---
+++ Kirbi/testing/current_testing/current_testing.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A21, INPUT);
+  analogReadResolution(13);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  double current = analogRead(A21).00805664;
+  Serial.println(current);
+  delay(100);
+}
