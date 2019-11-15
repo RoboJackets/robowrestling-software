29b0f9322d35fa4b11ad5c2abc88de78834b8763
Logan Schick <schicklogan@gmail.com>
2019-11-14 21:45:37 -0500

    CRC-16 sucks

--- Kirbi/testing/gucci_motor_testing/gucci_motor_testing.ino
+++ Kirbi/testing/gucci_motor_testing/gucci_motor_testing.ino
@@ -2,13 +2,15 @@
 #define ESC_R_SERIAL Serial3
 
 #define ESC_L_ADDRESS 0x80
-#define ESC_R_ADDRESS 0x81
+#define ESC_R_ADDRESS 0x80
 
 
 
 void setup() {
   // put your setup code here, to run once:
   ESC_L_SERIAL.begin(115200);
+  ESC_L_SERIAL.setRX(27);
+  ESC_L_SERIAL.setTX(26);
 }
 
 void loop() {
@@ -17,39 +19,22 @@ void loop() {
   bytes[1] = 0; //0 is drive forward
   bytes[2] = 63; //drive motor at half speed
   unsigned int crc;
-  for(int i = 0; i < 3; i++) {
-    crc = crc ^ (bytes[i] << 8);
-    for (int j = 0; j < 8; j++) {
-      if (crc & 0x8000) {
-        crc = (crc << 1) ^ 0x1021;
-      } else {
-        crc = crc << 1;
-      }
-    }
-  }
+//  for(int i = 0; i < 3; i++) {
+//    crc = crc ^ (bytes[i] << 8);
+//    for (int j = 0; j < 8; j++) {
+//      if (crc & 0x8000) {
+//        crc = (crc << 1) ^ 0x1021;
+//      } else {
+//        crc = crc << 1;
+//      }
+//    }
+//  }
   ESC_L_SERIAL.write(bytes[0]);
   ESC_L_SERIAL.write(bytes[1]);
   ESC_L_SERIAL.write(bytes[2]);
-  ESC_L_SERIAL.write(crc);
-  delay(100);
-  
-  bytes[0] = ESC_L_ADDRESS;
-  bytes[1] = 0; //0 is drive forward
-  bytes[2] = 0; //drive motor at half speed
-  crc = 0;
-  for(int i = 0; i < 3; i++) {
-    crc = crc ^ (bytes[i] << 8);
-    for (int j = 0; j < 8; j++) {
-      if (crc & 0x8000) {
-        crc = (crc << 1) ^ 0x1021;
-      } else {
-        crc = crc << 1;
-      }
-    }
-  }
-  ESC_L_SERIAL.write(bytes[0]);
-  ESC_L_SERIAL.write(bytes[1]);
-  ESC_L_SERIAL.write(bytes[2]);
-  ESC_L_SERIAL.write(crc);
-  delay(100);
+//  ESC_L_SERIAL.write((unsigned char)((crc >> 8) & 0xFF));
+//  ESC_L_SERIAL.write((unsigned char)(crc & 0xFF));
+  ESC_L_SERIAL.write(0x3A);
+  ESC_L_SERIAL.write(0x82);
+  delay(10);
 }
