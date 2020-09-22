0cf684bd9c0d27dc621821d0b52be912bc307bdc
Logan Schick <schicklogan@gmail.com>
2020-09-22 19:25:31 -0400

    Switched to regular pointers. Deletion should be correct. Compiles but on my computer when run window instantly closes

--- simulator/sim.cpp
+++ simulator/sim.cpp
@@ -1,46 +1,57 @@
 #include "sim.h"
 
-
-
-void initialize() {	
-	window.reset(new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My window"));
-	window->clear(sf::Color::White); // clear the window with white color
-	sf::CircleShape dohyo(150.f); //create the circle that represents the dohyo. Input is radius
+void draw_field() {
+    
+    sf::CircleShape dohyo(150.f); //create the circle that represents the dohyo. Input is radius
     dohyo.setFillColor(sf::Color(0, 0, 0)); //color the dohyo black
     dohyo.setOutlineThickness(10.f); //give dohyo an outline
     dohyo.setOutlineColor(sf::Color(250, 250, 250)); //make the outline white
     dohyo.setPosition((WINDOW_WIDTH/2)-150, (WINDOW_HEIGHT/2)-150);
     window->draw(dohyo);
+}
 
-    robot_rectangle1.reset(new sf::RectangleShape(sf::Vector2f(robot1->width, robot1->length)));
-    robot_rectangle1->setFillColor(sf::Color(50, 50, 50));
-    robot_rectangle1->setPosition(robot1->x_pos-robot1->width/2, robot1->y_pos-robot1->length/2);
-    window->draw(*robot_rectangle1);
-
-    robot_rectangle2.reset(new sf::RectangleShape(sf::Vector2f(robot2->width, robot2->length)));
-    robot_rectangle2->setFillColor(sf::Color(50, 50, 50));
-    robot_rectangle2->setPosition(robot2->x_pos-robot2->width/2, robot2->y_pos-robot2->length/2);
-    window->draw(*robot_rectangle2);
+void draw_robot(Robot* robot) {
+    sf::RectangleShape rectangle(sf::Vector2f(robot->width, robot->length));
+    rectangle.setFillColor(sf::Color(50, 50, 50));
+    rectangle.setPosition(robot->x_pos - robot->width/2, robot->y_pos - robot->length/2);
+    window->draw(rectangle);
 }	
 
 void update() {
 	sf::Event event;
+    window->clear(sf::Color::White); // clear the window with white color
     while (window->pollEvent(event)) {
         // "close requested" event: we close the window
         if (event.type == sf::Event::Closed)
             window->close();
+        delete window;
     }
-
+    draw_field();
+    draw_robot(robot1);
+    draw_robot(robot2);
     window->display();
 }
 
 int main() {
 	/* code */
-	robot1.reset(new BasicRobot((WINDOW_WIDTH/2)-65, WINDOW_HEIGHT/2, 90));
-	robot2.reset(new BasicRobot((WINDOW_WIDTH/2)+65, WINDOW_HEIGHT/2, 270));
-	initialize();
+	robot1 = new BasicRobot((WINDOW_WIDTH/2)-65, WINDOW_HEIGHT/2, 90);
+	robot2 = new BasicRobot((WINDOW_WIDTH/2)+65, WINDOW_HEIGHT/2, 270);
+    physics_updater = new RobotPhysicsUpdater();
+    
+    window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My window");
+    window->clear(sf::Color::White); // clear the window with white color
+
+
+    draw_field();
+    draw_robot(robot1);
+    draw_robot(robot2);
+    int i = 0;
 	while (window->isOpen()) {
+        if (i < 5) {
+            physics_updater->move_robot(robot1, 100, 100, 1);
+        }
 		update();
 	}
+    delete robot1, robot2, physics_updater;
 	return 0;
 }
