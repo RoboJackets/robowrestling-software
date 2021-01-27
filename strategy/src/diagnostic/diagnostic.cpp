f7b72ab36425811e9b99f36f126ac7eedcede3ec
ohawke <71109680+ohawke@users.noreply.github.com>
2021-01-26 19:47:36 -0500

    Update diagnostic.cpp

--- strategy/src/diagnostic/diagnostic.cpp
+++ strategy/src/diagnostic/diagnostic.cpp
@@ -1,10 +1,16 @@
 void checkMotor() {
-    int InitRight = right_encoder;
-    int InitLeft = left_encoder;
+    int init_right = right_encoder;
+    int init_left = left_encoder;
     drive(127,255);
     delay(5000);
     drive(0,0);
-    double RightSpeed = right_encoder/(4096*5);
-    double LeftSpeed = left_encoder/(4096*5);
-
+    double right_speed_f = (right_encoder-init_right)/(4096*5);
+    double left_speed_f = (left_encoder-init_left)/(4096*5);
+    init_right = right_encoder;
+    init_left = left_encoder;
+    drive(63,191);
+    delay(5000);
+    drive(0,0);
+    double right_speed_r = (right_encoder-init_right)/(4096*5);
+    double left_speed_r = (left_encoder-init_left)/(4096*5);
 }
