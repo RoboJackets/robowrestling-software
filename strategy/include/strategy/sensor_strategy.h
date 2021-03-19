c02c820d94215461a3b6237e1989ebb2b8aff898
ohawke <71109680+ohawke@users.noreply.github.com>
2021-03-18 20:40:40 -0400

    Update sensor_strategy.h

--- strategy/include/strategy/sensor_strategy.h
+++ strategy/include/strategy/sensor_strategy.h
@@ -1,8 +1,9 @@
 #include <strategy/strategy.h>
 
+
 class SensorStrategy: public Strategy {
     public:
         SensorStrategy();
-        std::vector<int> next_action(std::vector<double> inputs);
+        std::vector<int> next_action(DistanceBuffer d, LineBuffer l, EncoderBuffer e, ImuData i);
         int state_;
 };
