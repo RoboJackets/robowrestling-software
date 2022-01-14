f6b3ec0aa60e8c58f1c622c13088e066cde4ac39
koolk <koolkev246@gmail.com>
2022-01-13 19:28:57 -0500

    minor comments

--- strategy/src/kevin_brody.cpp
+++ strategy/src/kevin_brody.cpp
@@ -44,6 +44,10 @@ std::vector<int> KevinBrodyStrategy::next_action(SensorData input) {
 }
 
 KevinBrodyStrategy::State KevinBrodyStrategy::update_state(SensorData input) {
+    // TODO: implement, you can delete the stuff below
+    // input.dist_buffer_[2] and 3 are the front ones
+    // 0 and 1 are left
+    // 4 and 5 are right
     if (input.dist_buffer_[2] < 30 || input.dist_buffer_[3] < 30) { //front ones
         return FORWARD;
     } else if (input.dist_buffer_[2] < 150 || input.dist_buffer_[3] < 150) {
