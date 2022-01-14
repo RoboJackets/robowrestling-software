966e121d1f620097c90dbbe70ade6b640f2e14ef
koolk <koolkev246@gmail.com>
2022-01-13 19:26:17 -0500

    moving works, time to implement

--- strategy/src/kevin_brody.cpp
+++ strategy/src/kevin_brody.cpp
@@ -18,10 +18,16 @@ std::vector<int> KevinBrodyStrategy::next_action(SensorData input) {
         output[0] = -100;
         output[1] = -100;
         break;
+    // fast af turn
     case TURN_LEFT:
         output[0] = 0;
         output[1] = 100;
         break;
+    // turns in place
+    // case TURN_LEFT:
+    //     output[0] = -50;
+    //     output[1] = 50;
+    //     break;
     case TURN_RIGHT:
         output[0] = 100;
         output[1] = 0;
@@ -42,16 +48,15 @@ std::vector<int> KevinBrodyStrategy::next_action(SensorData input) {
 }
 
 KevinBrodyStrategy::State KevinBrodyStrategy::update_state(SensorData input) {
-    // if (input.dist_buffer_[2] < 30 || input.dist_buffer_[3] < 30) { //front ones
-    //     return FORWARD;
-    // } else if (input.dist_buffer_[2] < 150 || input.dist_buffer_[3] < 150) {
-    //     return TURN_LEFT;
-    // } else if ((input.line_buffer_[1] > 190 || input.line_buffer_[1] > 190)
-    //     && (input.dist_buffer_[0] < 150 || input.dist_buffer_[1] < 150) // left
-    //     && (input.dist_buffer_[4] < 150 || input.dist_buffer_[5] < 150)) { // right
-    //     return BACK;
-    // } else {
-    //     return TURN_RIGHT;
-    // }
-    return TURN_LEFT;
+    if (input.dist_buffer_[2] < 30 || input.dist_buffer_[3] < 30) { //front ones
+        return FORWARD;
+    } else if (input.dist_buffer_[2] < 150 || input.dist_buffer_[3] < 150) {
+         return TURN_LEFT;
+    } else if ((input.line_buffer_[1] > 190 || input.line_buffer_[1] > 190)
+         && (input.dist_buffer_[0] < 150 || input.dist_buffer_[1] < 150) // left
+         && (input.dist_buffer_[4] < 150 || input.dist_buffer_[5] < 150)) { // right
+         return BACK;
+    } else {
+         return FORWARD;
+    }
 }
