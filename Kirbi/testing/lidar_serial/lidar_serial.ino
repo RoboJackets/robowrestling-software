d0cbce405731f38123536963fbb37801108832a0
Rhea-Kadakia <rhea.r.kadakia@gmail.com>
2022-04-05 20:13:51 -0400

    Updated lidar code
    Updated lidar code

--- Kirbi/testing/lidar_serial/lidar_serial.ino
+++ Kirbi/testing/lidar_serial/lidar_serial.ino
@@ -5,7 +5,7 @@ byte byteArray [9];
 // 0x89  0x89 Dist_L  Dist_H Strength_L Strength_H Temp_L Temp_H Checksum
 // byte2 is distance, overflows into byte3
 
-byte configOutput [5] = {0x5A, 0x05, 0x07, 0x01, 0x11}; // write this array to HWSERIAL to enable output
+byte configOutput [5] = {0x5A, 0x05, 0x07, 0x01, 0x67}; // write this array to HWSERIAL to enable output
 byte configUART [5] = {0x5A, 0x05, 0x0A, 0x00, 0x11}; // write this array to HWSERIAL to set sensor to UART mode
 // more config commands on datasheet https://acroname.com/sites/default/files/assets/sj-gu-tfmini_plus-01-a04-datasheet_en.pdf
 
@@ -13,12 +13,13 @@ byte configUART [5] = {0x5A, 0x05, 0x0A, 0x00, 0x11}; // write this array to HWS
 void setup() {
   Serial.begin(115200); 
   Serial.println("Hello");
-  HWSERIAL.setRX(0);
-  HWSERIAL.setTX(1);
+  HWSERIAL.setRX(31);
+  HWSERIAL.setTX(32);
   HWSERIAL.begin(115200);// default baud rate
   while(!HWSERIAL) { delay(100); }
   HWSERIAL.write(configUART, 5); // set sensor to UART mode
   HWSERIAL.write(configOutput, 5); // enable output
+  delay(1000);
 }
 
 void loop() {
