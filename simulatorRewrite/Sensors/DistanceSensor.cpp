21eb3bdf3244406ac0588a3204e4d55dded81dfa
Matthew Stadter <matthewstadter@gmail.com>
2022-09-20 20:44:20 -0400

    Creating abstract Sensor Class

--- simulatorRewrite/Sensors/DistanceSensor.cpp
+++ simulatorRewrite/Sensors/DistanceSensor.cpp
@@ -4,4 +4,6 @@ class DistanceSensor : Sensor {
   double getInfo() {
     //custom logic :)
   }
+  double angle; //0 to 2pi, 0 is forward from the robot, counterclockwise like a unit circle
+  double lightRange; //the centimeter distance the lidar can shine -> The Dohyo's radius is 154cm
 };
--- simulatorRewrite/Sensors/Sensor.h
+++ simulatorRewrite/Sensors/Sensor.h
@@ -1,4 +1,15 @@
+#include <tuple>
+
 class Sensor {
-  virtual double getInfo() = 0;
-  
+  /**
+   * @brief Get the Info object
+   * 
+   * @param robotPos the position of the robot in meters defined from the bottom left
+   * @return double ??
+   */
+  virtual double getInfo(std::tuple<double> robotPos) = 0;
+  // 0 is the center of the robot
+  //location relative to placement on robot
+  double x;
+  double y; 
 };
