d16421da7c6ee0105d919fb8afbd4dfd45a75ebd
LawsOfScience <thesandwichsupreme@gmail.com>
2022-11-03 20:31:08 -0400

    Create basic Matador strategy code

---
+++ firmware/robot-firmware/src/Strategies/Matador/Matador.h
@@ -0,0 +1,17 @@
+#ifndef MATADOR_H
+#define MATADOR_H
+#include "Behaviors/Nodes/SequenceNode.h"
+
+struct MatadorState {
+
+};
+
+template<typename T>
+class Matador : public BT::SequenceNode<T> {
+    public:
+        Matador() {
+
+        }
+}
+
+#endif
