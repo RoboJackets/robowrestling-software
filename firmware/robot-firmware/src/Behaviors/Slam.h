ed5de101524a00ff103888f58d6d9931fb7e727d
Keon Sanavandi <keonsanavandijoon@gmail.com>
2022-12-01 19:39:11 -0500

    Added slam.h

---
+++ firmware/robot-firmware/src/Behaviors/Slam.h
@@ -0,0 +1,29 @@
+lines (20 sloc)  528 Bytes
+
+#ifndef SLAM_H
+#define SLAM_H
+
+#include "Nodes/Node.h"
+#include "MotorController/MotorController.h"
+
+template<typename T, typename U>
+class Slam : public BT::Node<T, U> {
+public:
+    Slam(int speed) : BT::Node<T, U>(0, 0), speed(speed){
+        
+    }
+
+    U Run(T inputs) override {
+        this->_finished = inputs.lidars[2] > 1000 || inputs.lidars[3] > 1000; // neither lidars see anything
+
+        U out;
+        out.motor1 = speed;
+        out.motor2 = speed;
+        return out;
+    }
+
+protected:
+    int speed;
+};
+
+#endif
