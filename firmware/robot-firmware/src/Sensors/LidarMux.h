791a8cc66e790583e4875d7f508e7357bcf0fffe
Matthew Stadter <matthewstadter@gmail.com>
2023-02-02 19:27:44 -0500

    LidarMux code complete, includes example

--- firmware/robot-firmware/src/Sensors/LidarMux.h
+++ firmware/robot-firmware/src/Sensors/LidarMux.h
@@ -10,6 +10,32 @@ struct twoValues {
     int lidarTwo;
 };
 
+
+/***
+ * This is a class to control the muxes attached to two lidars. 
+ * 
+ * 
+ * 
+ * Example code to read from both lidars (attached to a mux) in a loop
+        Before Setup:
+            TFMini* tfMini;
+            LidarMux* mux1;
+        Setup:
+            tfMini = new TFMini(9, 10);
+             mux1 = new LidarMux(25, tfMini);
+        Before Loop:
+            String out1 = "Lidar one: ";
+            String out2 = ", Lidar two: ";
+            struct twoValues* x = new twoValues;
+        In Loop:
+            mux1->readLidars(x);
+            String out =  out1 + x->lidarOne + out2 + x->lidarTwo;
+            Serial.println(out);
+}
+*/
+
+
+
 class LidarMux {
     private:
         int _muxPin;
@@ -25,7 +51,15 @@ class LidarMux {
         LidarMux(int muxPin, TFMini* tfmini) {
             _tfmini = tfmini;
             _muxPin = muxPin;
-            pinMode(_muxPin, INPUT);
+            pinMode(_muxPin, OUTPUT);
+            setLow();
+        }
+
+        LidarMux(int muxPin, int rx, int tx) {
+            _tfmini = new TFMini(rx, tx);
+            _muxPin = muxPin;
+            pinMode(_muxPin, OUTPUT);
+            setLow();
         }
 
         struct twoValues* readLidars() {
@@ -50,6 +84,25 @@ class LidarMux {
             int two = _tfmini->GetDistance();
             pointer->lidarOne = one;
             pointer->lidarTwo = two;
+            return pointer;
+        }
+        /**
+         * Use this if need clocking?
+         * 
+        */
+        struct twoValues* readLidars(struct twoValues* pointer, int clock) {
+            if (!clock) {
+                setLow();
+                 _tfmini->Poll();
+                 int one = _tfmini->GetDistance();
+                  pointer->lidarOne = one;
+            }
+            else {
+                setHigh();
+                _tfmini->Poll();
+                int two = _tfmini->GetDistance();
+                pointer->lidarTwo = two;
+            }           
             return pointer;
         }
 
--- firmware/robot-firmware/src/main.cpp
+++ firmware/robot-firmware/src/main.cpp
@@ -3,23 +3,33 @@
 #include <memory>
 
 #include "MotorController/MotorController.h"
-#include <TFMPlus.h>
-
+//#include <TFMPlus.h>
+#include "Sensors/TFMini/TFMini.h"
+#include "Sensors/LidarMux.h"
 
 // Init motor controller
 // Make sure that everything is grounded
 SoftwareSerial* motorControllerSerial = new SoftwareSerial(11, 2); // tx on pin 2, rx on pin 11 (not needed)
 MotorController motorController{motorControllerSerial};
+TFMini* tfMini;
+LidarMux* mux1;
 
 void setup() {
+  tfMini = new TFMini(9, 10);
+  mux1 = new LidarMux(25, tfMini);
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial1.begin(115200);
   Serial2.begin(115200);
-  motorControllerSerial -> begin(9600);
-}
+  //motorControllerSerial -> begin(9600);
 
+}
+String out1 = "Lidar one: ";
+String out2 = ", Lidar two: ";
+struct twoValues* x = new twoValues;
 void loop() {
-  motorController.move(50);
+  mux1->readLidars(x);
+  String out =  out1 + x->lidarOne + out2 + x->lidarTwo;
+  Serial.println(out);
 }
 
