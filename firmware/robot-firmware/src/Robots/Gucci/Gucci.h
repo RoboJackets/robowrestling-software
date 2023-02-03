1493af91602a49cee8b1bc7aca56714b186a13a9
Andrew Cua <andrew@cuafamily.com>
2023-02-02 19:27:01 -0500

    LineSensor extends from Sensor base class

--- firmware/robot-firmware/src/Robots/Gucci/Gucci.h
+++ firmware/robot-firmware/src/Robots/Gucci/Gucci.h
@@ -4,12 +4,18 @@
 #include "Robots/Robot.h"
 #include <Arduino.h>
 #include "Sensors/TFMini/TFMini.h"
+#include "Sensors/LineSensor.h"
 
 class Gucci : public Robot {
 private: 
         TFMini* rightDist;
         TFMini* frontDist;
         TFMini* leftDist;
+
+        LineSensor* lineSensor1;
+        LineSensor* lineSensor2;  
+
+
 public: 
     Gucci() {
         //TODO: replace these with the actual pin values
@@ -30,16 +36,22 @@ public:
         frontDist = new TFMini(&Serial2); 
         leftDist =  new TFMini(&Serial3); 
 
+        lineSensor1 = new LineSensor(23);
+        lineSensor2 = new LineSensor(35);
+
         _sensors.push_back(rightDist); // right 
         _sensors.push_back(frontDist); 
         _sensors.push_back(leftDist); 
+
+        _sensors.push_back(lineSensor1); 
+        _sensors.push_back(lineSensor2);
 
     }
 
     void UpdateState() override {
         
-        Serial.printf("left: %d, right: %d, front: %d \n", leftDist->GetDistance(), rightDist->GetDistance(), frontDist->GetDistance()); 
-    
+        //Serial.printf("left: %d, right: %d, front: %d \n", leftDist->GetDistance(), rightDist->GetDistance(), frontDist->GetDistance()); 
+        //Serial.printf("LineSensor1: %d, LineSensor2: %d \n", lineSensor1->GetDetection(), lineSensor2->GetDetection());
     } 
 
 
--- firmware/robot-firmware/src/Sensors/LineSensor.h
+++ firmware/robot-firmware/src/Sensors/LineSensor.h
@@ -2,16 +2,27 @@
 #define LINE_SENSOR_H
 
 #include <Arduino.h>
+#include "Sensors/Sensor.h"
 
-class LineSensor {
+class LineSensor : public Sensor {
 public:
     LineSensor(int pin) : _pin(pin) {}
     ~LineSensor() {}
 
     int read() { return analogRead(_pin); }
+    
+   bool Poll() override {
+        _detected = analogRead(_pin); 
+        return true; 
+   }
+
+   int GetDetection() {
+        return _detected; 
+   }
 
 private:
     int _pin;
+    int _detected; 
 };
 
 #endif
