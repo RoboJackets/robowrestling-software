a168e3dd91f8b639ef949bddfa20f4ebdbc06e19
Andrew Cua <andrew@cuafamily.com>
2023-02-21 20:32:06 -0500

    Fix merge issues

--- firmware/robot-firmware/src/main.cpp
+++ firmware/robot-firmware/src/main.cpp
@@ -12,32 +12,29 @@
 // Make sure that everything is grounded
 SoftwareSerial* motorControllerSerial = new SoftwareSerial(11, 2); // tx on pin 2, rx on pin 11 (not needed)
 MotorController motorController{motorControllerSerial};
-<<<<<<< Updated upstream
 TFMini* tfMini;
 LidarMux* mux1;
 Gucci gucci{};
-=======
-Gucci gucci{};  
->>>>>>> Stashed changes
 int16_t dist = -1;
 void setup() {
+  tfMini = new TFMini(&Serial2);
+  mux1 = new LidarMux(25, tfMini);
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  Serial2.begin(115200);
+  Serial3.begin(115200); 
   //motorControllerSerial -> begin(9600);
 
 }
-
+String out1 = "Lidar one: ";
+String out2 = ", Lidar two: ";
+struct twoValues* x = new twoValues;
 void loop() {
-<<<<<<< Updated upstream
   Serial.println("======= NEW LOOP AAAA =======");
   mux1->readLidars(x);
   String out =  out1 + x->lidarOne + out2 + x->lidarTwo;
   Serial.println(out);
-  gucci.UpdateSensors();
+  //gucci.UpdateSensors();
   delay(5000);
-=======
-   
-  gucci.UpdateSensors(); 
-   
->>>>>>> Stashed changes
 }
-
