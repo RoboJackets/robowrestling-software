895349fdb96034c436d930cccced8c282c9cc3af
LawsOfScience <thesandwichsupreme@gmail.com>
2023-06-13 11:24:12 -0400

    Reorganize firmware folders - Robot code now classified by size and robot name - Common firmware extracted to firmware/CommonFirmware

--- firmware/3kg/Kirbi/src/Strategies/SlammyWhammy/SlammyWhammy.h
+++ firmware/3kg/Kirbi/src/Strategies/SlammyWhammy/SlammyWhammy.h
@@ -14,33 +14,33 @@ class SlammyWhammy : public BT::Node<T, U> {
 public:
     SlammyWhammy(int turnSpeed, int slamSpeed) : BT::Node<T,U>(0, 0) {
         search = new Search<T,U>(100);
-        figureEight = new FigureEight<T,U>(75, 850); 
+        figureEight = new FigureEight<T,U>(75, 850);
         slam = new Slam<T,U>(slamSpeed);
     }
 
 
     U Run(T state) {
-        // U out; 
-        // out.currentLeftMotorPow = 0; 
-        // out.currentRightMotorPow = 0; 
+        // U out;
+        // out.currentLeftMotorPow = 0;
+        // out.currentRightMotorPow = 0;
         // if (state.lidars[2] > 150 || state.lidars[3] > 150) {
-        //     return out; 
+        //     return out;
         // }
 
         if ((state.lidars[2] > 150 || state.lidars[3] > 150) && !(millis() - lockout < LOCKOUT_TIME)) {
             return search->Run(state);
         } else {
-            lockout = millis(); 
+            lockout = millis();
         }
 
-        return slam->Run(state); 
+        return slam->Run(state);
     }
 
 private:
     Search<T,U>* search;
     Slam<T,U>* slam;
     FigureEight<T,U>* figureEight;
-    unsigned int lockout = 0; 
+    unsigned int lockout = 0;
 };
 
 #endif
---
+++ firmware/README.md
@@ -0,0 +1,5 @@
+## Folders
+
+- `CommonFirmware`: contains firmware modules that multiple bots use, organized into 3kg and 500g
+- `3kg`: contains all code for 3kg bots, separated into folders for each bot
+- `500g`: contains all code for 500g bots, separated into folders for each bot
