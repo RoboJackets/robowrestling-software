6bc68c9afd4eed1623c44b3b6a262eef3ac6b435
Xander Riddle <xriddle0917@gmail.com>
2024-06-23 16:56:03 -0400

    implemented Sensor Class

---
+++ robots/Ferrari/src/Sensors/LineSensor.cpp
@@ -0,0 +1,17 @@
+#include "Sensors/LineSensor.h"
+
+LineSensor::LineSensor() {
+    value = 0;
+}
+
+LineSensor::LineSensor(int sensorValue) {
+    value = sensorValue;
+}
+
+int LineSensor::getValue() {
+    return value;
+}
+
+void LineSensor::setValue(int sensorValue) {
+    value = sensorValue;
+}
