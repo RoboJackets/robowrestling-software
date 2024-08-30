57cdc5ca3ae21fee9a6f574e5953e7bb89d260c3
LawsOfScience <thesandwichsupreme@gmail.com>
2024-08-29 22:04:56 -0400

    Initial Skibidi commit
    This name definitely won't haunt any of us

---
+++ 3kg/skibidi/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup(void) {
+    // Serial initialization (for debugging)
+    // Pin initialization
+    // State initialization
+    // Sensor initialization
+}
+
+void loop(void) {
+    // Update sensors
+    // Make decision
+    // Execute decision
+}
