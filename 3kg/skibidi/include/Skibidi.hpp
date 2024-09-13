67547937cc381e28fa53253143039f3e6ca4410b
LawsOfScience <thesandwichsupreme@gmail.com>
2024-09-12 21:34:13 -0400

    Add IrSensor and StartModule into Skibidi

--- 3kg/skibidi/include/Skibidi.hpp
+++ 3kg/skibidi/include/Skibidi.hpp
@@ -4,12 +4,16 @@
 #include <map>
 #include <utility>
 #include "Sensors/DoubleLineSensor.hpp"
+#include "Sensors/IrSensor.hpp"
+#include "Sensors/StartModule.hpp"
 
 enum Position { FRONT_LEFT, FRONT_RIGHT, BACK_LEFT, BACK_RIGHT };
 
 class Skibidi {
     private:
         std::map<Position, DoubleLineSensor*> line_sensors;
+        std::pair<IrSensor*, IrSensor*> ir_sensors;
+        StartModule* start_module;
     public:
         Skibidi();
 };
--- 3kg/skibidi/src/Skibidi.cpp
+++ 3kg/skibidi/src/Skibidi.cpp
@@ -5,4 +5,8 @@ Skibidi::Skibidi() {
     this->line_sensors.insert(std::make_pair(Position::FRONT_RIGHT, new DoubleLineSensor(1)));
     this->line_sensors.insert(std::make_pair(Position::BACK_RIGHT, new DoubleLineSensor(1)));
     this->line_sensors.insert(std::make_pair(Position::BACK_RIGHT, new DoubleLineSensor(1)));
+
+    this->ir_sensors = std::make_pair(new IrSensor(1), new IrSensor(1));
+
+    this->start_module = new StartModule(1);
 }
