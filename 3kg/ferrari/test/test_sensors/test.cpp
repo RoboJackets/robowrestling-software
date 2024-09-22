1f3cffaf06d5d383972745943969f1c0867b26c4
Xander Riddle <xriddle0917@gmail.com>
2024-09-22 15:21:01 -0400

    added unit testing layout

---
+++ 3kg/ferrari/test/test_sensors/test.cpp
@@ -0,0 +1,17 @@
+#include "unity.h"
+#include "sensors/IRSensor.h"
+#include "sensors/LineSensor.h"
+
+void setUp(void) { }
+void tearDown(void) { }
+
+/* asserts true is true */
+void test_test(void) {
+    TEST_ASSERT_TRUE(true);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_test);
+    return UNITY_END();
+}
