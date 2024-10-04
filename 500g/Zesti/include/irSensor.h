6162bf55c31521b37f06c90c1684d93fc6b900ff
Dulolu <kevink3535@gmail.com>
2024-10-03 21:04:01 -0400

    Sensor

---
+++ 500g/Zesti/include/irSensor.h
@@ -0,0 +1,13 @@
+#ifndef IRSENSOR_H
+#define IRSENSOR_H
+
+class irSensor {
+    private: 
+        bool irDetected;
+    public: 
+        irSensor();
+        bool getirSensor();
+        void setirSensor(bool value);
+};
+
+#endif
---
+++ 500g/Zesti/include/lineSensor.h
@@ -0,0 +1,13 @@
+#ifndef LINESENSOR_H
+#define LINESENSOR_H
+
+class lineSensor {
+    private: 
+        bool lineDetected;
+    public: 
+        lineSensor();
+        bool getLineSensor();
+        void setLineSensor(bool value);
+};
+
+#endif
