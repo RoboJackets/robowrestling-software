ef5efc899ab79a0468abddd8d797241637e6a576
JesseZhang0 <jessezhang0@gmail.com>
2024-10-10 19:53:40 -0400

    added ir_sensor

---
+++ 500g/Celebi/src/sensors/ir_sensor.cpp
@@ -0,0 +1,17 @@
+#include "sensors/ir_sensor.hpp"
+
+ir_sensor :: ir_sensor() {
+    ir_sense = false;
+}
+
+ir_sensor :: ir_sensor(bool init_value) {
+    ir_sense = init_value;
+}
+
+bool ir_sensor :: get_ir_sense() {
+    return ir_sense;
+}
+
+void ir_sensor :: set_ir_sense(bool new_value) { 
+    ir_sense = new_value;
+}
