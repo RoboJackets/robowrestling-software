52ac5e23923414b29b8ae58c2a5989c648403760
Muliashia, Rit <rmuliashia3@gatech.edu>
2024-10-10 20:12:04 -0400

    Made the folder and header files

---
+++ 500g/pharmaci/src/irSensor.cpp
@@ -0,0 +1,9 @@
+#include "irSensor.h"
+irSensor::irSensor() : irDetected(false) {
+}
+bool irSensor::getirSensor() {
+    return irDetected;
+}
+void irSensor::setirSensor(bool value) {
+    irDetected = value;
+}
