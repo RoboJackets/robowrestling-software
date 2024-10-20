3fd224048994381e9df43237203eb18389444dc2
andrewli <andy.li3964@gmail.com>
2024-10-20 15:39:44 -0400

    fixed WorldState constructor to include sensors

--- 3kg/monki/monki_test/include/state/WorldState.h
+++ 3kg/monki/monki_test/include/state/WorldState.h
@@ -1,16 +1,19 @@
 #include <Arduino.h>
-#include "../sensors/IRSensor.h"
-#include "../sensors/LineSensor.h"
+#include "../common.h"
 
 #ifndef WORLD_STATE_H
 #define WORLD_STATE_H
 
 class WorldState {
     private:
-        IrSensor ir_sensors[5];
-        LineSensor line_sensors[2];
+        IrSensor ir_sensors[9];
+        LineSensor line_sensors[4];
     public:
-        
+        WorldState() : ir_sensors{IrSensor(), IrSensor(), IrSensor(), IrSensor(), IrSensor(), 
+                                  IrSensor(), IrSensor(), IrSensor(), IrSensor()}, 
+                       line_sensors{LineSensor(), LineSensor(), LineSensor(), LineSensor()} {
+            // Constructor body (if needed)
+        }
         int getEnemyPos();
         bool getAtBoundary();
 };
