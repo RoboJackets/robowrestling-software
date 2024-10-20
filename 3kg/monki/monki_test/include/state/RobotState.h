b0b265a976b37a48f7e5461e9c080c5760f04428
andrewli <andy.li3964@gmail.com>
2024-10-20 16:00:39 -0400

    code cleanup, beginning to instantiate objects in appropriate files

--- 3kg/monki/monki_test/include/state/RobotState.h
+++ 3kg/monki/monki_test/include/state/RobotState.h
@@ -1,13 +1,18 @@
 #include <Arduino.h>
-#include "../action/RobotAction.h"
+#include "../common.h"
 
 #ifndef ROBOT_STATE_H
 #define ROBOT_STATE_H
 
 class RobotState {
-    
+
+    // maybe include a separate robotstate struct
+
     private:
-       RobotAction robotAction; 
+        RobotAction robotAction; 
+        StartModule startModule;
+        CurrentSensor currentSensor;
+        IMU imu;
 
     public:
         void calculateState();
--- 3kg/monki/monki_test/src/main.cpp
+++ 3kg/monki/monki_test/src/main.cpp
@@ -1,18 +1,12 @@
 #include <Arduino.h>
+#include "../include/common.h">
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  RobotState monki_see;
+  RobotAction monki_do;
+  WorldState monki_world;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
