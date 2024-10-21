c041400a9c21f9152839416d237676443d61770f
andrewli <andy.li3964@gmail.com>
2024-10-21 17:40:58 -0400

    IR Sensor and Line Sensor class implementation

--- 3kg/monki/monki_test/include/sensors/IRSensor.h
+++ 3kg/monki/monki_test/include/sensors/IRSensor.h
@@ -3,6 +3,12 @@
 #ifndef IR_SENSOR_H
 #define IR_SENSOR_H
 
+// Omron E3Z-D82 model
+// PNP output, sensing distance 1 meter, pre-wired
+// PNP - output pin provides a positive voltage when the sensor detects an object
+// diffuse reflective sensing method (reflect ir light off object back to sensor)
+// light source: IR
+
 class IrSensor {
 
     private:
