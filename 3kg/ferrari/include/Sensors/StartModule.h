b169c76d55f419799bf4f9df2fcc1c92529f2173
Neel <neel468167@gmail.com>
2024-11-16 09:55:24 -0500

    ferrariUpdates

--- 3kg/ferrari/include/Sensors/StartModule.h
+++ 3kg/ferrari/include/Sensors/StartModule.h
@@ -1,23 +1,10 @@
 #ifndef START_MODULE_H
 #define START_MODULE_H
-#include <Arduino.h>
 
 class StartModule {
 public:
-    StartModule(int pin) : _pin(pin) {
-        pinMode(_pin, INPUT);
-    }
-    ~StartModule() {}
-    bool isActive() {
-        return digitalRead(_pin);
-    }
-    
-    int getVal() {
-        return analogRead(_pin);
-    }
-
-private:
-    int _pin;
+    StartModule();
+    int getVal();
 };
 
 #endif
