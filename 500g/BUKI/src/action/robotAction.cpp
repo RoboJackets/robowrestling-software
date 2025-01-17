9841ad6ad8ab8c029e55b2b2cdc45de280ece1fc
snaseem8 <snaseem8@gatech.edu>
2025-01-16 20:57:57 -0500

    uodated robotAction.cpp and robotState.cpp

---
+++ 500g/BUKI/src/action/robotAction.cpp
@@ -0,0 +1,25 @@
+#include "action/robotAction.h"
+
+RobotAction::RobotAction() {
+    
+}
+
+void RobotAction::turnLeft(MotorDriver** motors) {
+    
+}
+
+void RobotAction::turnRight(MotorDriver** motors) {
+    
+}
+
+void RobotAction::go(MotorDriver** motors) {
+    
+}
+
+void RobotAction::GOGOGO(MotorDriver** motors) {
+    
+}
+
+void RobotAction::reverse(MotorDriver** motors) {
+    
+}
