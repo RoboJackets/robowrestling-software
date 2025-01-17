53ffd24dae826a7c8941020eae6f88bdeddc8d8f
lobitzb564 <lo.crazyred@gmail.com>
2025-01-16 19:53:35 -0500

    Added Shell for robotstate, worldstate, robotaction

---
+++ 500g/BUKI/include/action/robotAction.h
@@ -0,0 +1,10 @@
+#ifndef ROBOT_ACTION_H_
+#define ROBOT_ACTION_H_
+
+class RobotAction {
+    
+    public:
+        RobotAction();
+};
+
+#endif
---
+++ 500g/BUKI/include/states/worldState.h
@@ -0,0 +1,10 @@
+#ifndef WORLD_STATE_H_
+#define WORLD_STATE_H_
+
+class WorldState {
+
+    public:
+        WorldState();
+};
+
+#endif
