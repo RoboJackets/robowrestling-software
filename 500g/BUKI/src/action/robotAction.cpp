644f172aa9d5e9de6b03e86e441ebe20074c6834
snaseem8 <snaseem8@gatech.edu>
2025-01-23 20:38:30 -0500

    Added code to control motors in robotAction.cpp

--- 500g/BUKI/src/action/robotAction.cpp
+++ 500g/BUKI/src/action/robotAction.cpp
@@ -1,25 +1,46 @@
 #include "action/robotAction.h"
 
 RobotAction::RobotAction() {
-    
+    // initialize empty motor drivers
+
 }
 
 void RobotAction::turnLeft(MotorDriver** motors) {
-    
+    // call setDir and setSpeed
+    motors[0]->setDir(true);
+    motors[1]->setDir(false);
+    motors[0]->setSpeed(125);
+    motors[1]->setSpeed(125);
 }
 
 void RobotAction::turnRight(MotorDriver** motors) {
-    
+    // call setDir and setSpeed
+    motors[0]->setDir(false);
+    motors[1]->setDir(true);
+    motors[0]->setSpeed(125);
+    motors[1]->setSpeed(125);
 }
 
 void RobotAction::go(MotorDriver** motors) {
-    
+    // call setDir and setSpeed
+    motors[0]->setDir(true);
+    motors[1]->setDir(true);
+    motors[0]->setSpeed(125);
+    motors[1]->setSpeed(125);
 }
 
 void RobotAction::GOGOGO(MotorDriver** motors) {
-    
+    // call setDir and setSpeed
+    motors[0]->setDir(true);
+    motors[1]->setDir(true);
+    motors[0]->setSpeed(255);
+    motors[1]->setSpeed(255);
 }
 
 void RobotAction::reverse(MotorDriver** motors) {
-    
+    // call setDir and setSpeed
+    motors[0]->setDir(false);
+    motors[1]->setDir(false);
+    motors[0]->setSpeed(125);
+    motors[1]->setSpeed(125);
 }
