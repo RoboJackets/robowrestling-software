2148b5513ce529289b35516154266a90698217f3
Playa490 <kamarowen2@gmail.com>
2025-01-23 20:44:20 -0500

    Added Motor Driver and Robot Action

---
+++ 500g/Oni/src/actions/robot_action.cpp
@@ -0,0 +1,94 @@
+#include "../include/actions/robot_action.h"
+
+
+//PLACE HOLDER VALUES UNTIL WE CAN TEST HOW MUCH IT ACTUALLY BE DOING TYPE SHITTTTTTTTTT
+//Working under assumptions that 1 means forward and 0 means backwards for direction
+
+// FORWARD
+void RobotAction::smallForward() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(1);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(1);
+}
+void RobotAction::medForward() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(2);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(2);
+}
+void RobotAction::largeForward() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(3);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(3);
+}
+
+// BACKWARD
+void RobotAction::smallBack() {
+    leftMotorDriver -> setDirection(0);
+    leftMotorDriver -> setSpeed(1);
+    rightMotorDriver -> setDirection(0);
+    rightMotorDriver -> setSpeed(1);
+}
+void RobotAction::medBack() {
+    leftMotorDriver -> setDirection(0);
+    leftMotorDriver -> setSpeed(2);
+    rightMotorDriver -> setDirection(0);
+    rightMotorDriver -> setSpeed(2);
+}
+void RobotAction::largeBack() {
+    leftMotorDriver -> setDirection(0);
+    leftMotorDriver -> setSpeed(3);
+    rightMotorDriver -> setDirection(0);
+    rightMotorDriver -> setSpeed(3);
+}
+
+// LEFT
+void RobotAction::smallLeft() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(1);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(2);
+}
+void RobotAction::medLeft() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(1);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(3);
+}
+void RobotAction::largeLeft() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(1);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(4);
+}
+
+// RIGHT
+void RobotAction::smallRight() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(2);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(1);
+}
+void RobotAction::medRight() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(3);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(1);
+}
+void RobotAction::largeRight() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(4);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(1);
+}
+
+// STOP
+
+void RobotAction::stop() {
+    leftMotorDriver -> setDirection(1);
+    leftMotorDriver -> setSpeed(0);
+    rightMotorDriver -> setDirection(1);
+    rightMotorDriver -> setSpeed(0);
+}
