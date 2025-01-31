a7b1dae7576572971b70e3b047f16c7725ed6226
snaseem8 <snaseem8@gatech.edu>
2025-01-30 19:43:16 -0500

    added Joe's main code to main.cpp

--- 500g/BUKI/src/main.cpp
+++ 500g/BUKI/src/main.cpp
@@ -1,13 +1,85 @@
 #include <Arduino.h>
 
+#define Lside 12
+#define Lsensor 8
+#define Rside 2
+#define Rsensor 4
+#define MSensor 7
+#define StartMod 10 
+
+#define Rpos 6
+#define Rneg 11
+#define Lpos 3
+#define Lneg 5
+#define switch1 A6
+#define switch2 A7
+void line();
+void stop();
+void turnLeft(double);
+void turnRight(double); 
+void back(double);
+void forward(double);
+void rotateRight(double);
+void rotateLeft(double);
+
 void setup() {
-    pinMode(2, OUTPUT);
+  pinMode(Rpos, OUTPUT); 
+  pinMode(Rneg, OUTPUT);
+  pinMode(Lpos, OUTPUT); 
+  pinMode(Lneg, OUTPUT);
+  pinMode(StartMod, INPUT);
+
+  pinMode(switch1, INPUT);
+  pinMode(switch2, INPUT);
+  Serial.begin(9600);
 }
 
-
 void loop() {
-    digitalWrite(2, HIGH);
-    delay(1);
-    digitalWrite(2, LOW);
-    delay(1);
+  if(digitalRead(MSensor))
+    forward(100);
+  else 
+    stop(); 
+
+}
+void stop() {
+  analogWrite(Rpos, 0);
+  analogWrite(Rneg, 0);
+  analogWrite(Lpos, 0);
+  analogWrite(Lneg, 0);
+}
+void forward(double speed) {
+  analogWrite(Rpos, speed+15);
+  analogWrite(Rneg, 0);
+  analogWrite(Lpos, speed);
+  analogWrite(Lneg, 0);
+}
+void back(double speed) {
+  analogWrite(Rpos, 0);
+  analogWrite(Rneg, speed+15);
+  analogWrite(Lpos, 0);
+  analogWrite(Lneg, speed);
+}
+void rotateRight(double speed) {
+  analogWrite(Rpos, 0);
+  analogWrite(Rneg, speed+10);
+  analogWrite(Lpos, speed);
+  analogWrite(Lneg, 0);
+}
+void rotateLeft(double speed) {
+  analogWrite(Rpos, speed+10);
+  analogWrite(Rneg, 0);
+  analogWrite(Lpos, 0);
+  analogWrite(Lneg, speed);
+}
+void turnRight(double speed) {
+  analogWrite(Rpos, 0);
+  analogWrite(Rneg, 0);
+  analogWrite(Lpos, speed);
+  analogWrite(Lneg, 0);
+}
+void turnLeft(double speed) {
+  analogWrite(Rpos, speed+10);
+  analogWrite(Rneg, 0);
+  analogWrite(Lpos, 0);
+  analogWrite(Lneg, 0);
 }
