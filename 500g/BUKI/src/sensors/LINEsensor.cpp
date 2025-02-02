26746fc44a6dc21b5d6cf99da252f80fe34890ed
snaseem8 <snaseem8@gatech.edu>
2025-02-02 14:04:47 -0500

    added comments to describe code functionality

--- 500g/BUKI/src/sensors/LINEsensor.cpp
+++ 500g/BUKI/src/sensors/LINEsensor.cpp
@@ -1,17 +1,17 @@
 #include "sensors/LINEsensor.h"
 
-LINEsensor::LINEsensor() {
+LINEsensor::LINEsensor() {      // constructor
     value = 0;
 }
 
-LINEsensor::LINEsensor(bool sensorValue) {
+LINEsensor::LINEsensor(bool sensorValue) {      // constructor
     value = sensorValue;
 }
 
-bool LINEsensor::getValue() {
+bool LINEsensor::getValue() {          // gets line sensor value (function)
     return value;
 }
 
-void LINEsensor::setValue(bool sensorValue) {
+void LINEsensor::setValue(bool sensorValue) {       // sets line sensor value (function)
     value = sensorValue;
 }
