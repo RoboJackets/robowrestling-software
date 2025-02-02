e2f9a40359d17851d1ba2f9e9274d4cc6cbef06c
snaseem8 <snaseem8@gatech.edu>
2025-02-02 15:23:41 -0500

    added private member (robotAction pointer) to .h and wrote code for the constructor and runAlgorithm function in .cpp

--- 500g/BUKI/include/states/moveForward.h
+++ 500g/BUKI/include/states/moveForward.h
@@ -2,11 +2,15 @@
 #define MOVE_FORWARD_H_
 
 #include "states/robotState.h"
+#include "action/robotAction.h"
+#include "states/worldState.h"
 
-class MoveForward : public RobotState {         // Polymorphism -> GeeksforGeeks
+class MoveForward : public RobotState {         // Polymorphism (I have no idea what's going on here - Shahmeel)
+    private:
+        RobotAction* robotAction;
     public:
-        MoveForward();
-        void runAlgorithm() override;
+        MoveForward(RobotAction* robotActionParameter);     // constructor
+        void runAlgorithm() override;           // Override runAlgorithm() from RobotState
 };
 
 #endif
--- 500g/BUKI/src/states/moveForward.cpp
+++ 500g/BUKI/src/states/moveForward.cpp
@@ -1,9 +1,9 @@
 #include "states/moveForward.h"
 
-MoveForward::MoveForward() {
-
+MoveForward::MoveForward(RobotAction* robotActionParameter) {       // constructor for MoveForward class. Takes a robotAction parameter (pointer to instance of robotAction in main) and assigns it to robotAction member variable
+    robotAction = robotActionParameter;
 }
 
-void MoveForward::runAlgorithm() {
-
+void MoveForward::runAlgorithm() {      // calls runAlgorithm method of MoveForward class. Delegates movement of robot to robotAction object
+    robotAction->go();                  // calls go() method on robotAction object
 }
