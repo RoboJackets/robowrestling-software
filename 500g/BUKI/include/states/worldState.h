6a881ec633735dcb06a77f570d30d21bab65db2e
snaseem8 <snaseem8@gatech.edu>
2025-02-02 14:47:59 -0500

    Added two enumerations (SLIGHT_LEFT and SLIGHT_RIGHT) for IR sensors. Changed isNearEdge and enemyPos to return "Edge" enumeration. Changed enemyPos function to use enumerations instead of raw array values.

--- 500g/BUKI/include/states/worldState.h
+++ 500g/BUKI/include/states/worldState.h
@@ -6,24 +6,24 @@
 
 enum Edge {
     LEFT,
+    SLIGHT_LEFT,
     RIGHT,
+    SLIGHT_RIGHT,
     FRONT,
     BACK,
     SAFE
 };
 
-// add enumerations and integrate with functions. Ex: enumeration: EnemyPosition, and then EnemyPosition getEnemyPosition
-
 class WorldState {
     private:
-        LINEsensor** lnsn;          // pass array of pointers for IR and Line sensors
-        IRsensor** irsn;
+        LINEsensor** lnsn;          // pointer to array of line sensors
+        IRsensor** irsn;            // pointer to aray of IR sensors
     public:
-        WorldState(LINEsensor*[], IRsensor*[]);
-        short isNearEdge();
-        int enemyDistance();
-        void readSensors();
-        void enemyPos(int*);    
+        WorldState(LINEsensor*[], IRsensor*[]);     // Constructor: Takes arrays of pointers to LINEsensor and IRsensor objects
+        Edge isNearEdge();                         // Function to check if the robot is near an edge and return the corresponding enum value
+        int enemyDistance();                        
+        void readSensors();                         // Function to read values from all sensors and update the world state
+        Edge enemyPos();                            // Function to check enemy Position and return enum value
 };
 
 #endif
