f1d598ec958b674e92b93459490e3e3994b86932
lobitzb564 <lo.crazyred@gmail.com>
2025-02-13 19:20:07 -0500

    new code for wasabi

--- 500g/BUKI/mains/joes_main/main.cpp
+++ 500g/BUKI/mains/joes_main/main.cpp
@@ -4,95 +4,112 @@
  * 1/30/2025
  */
 
-#include <Arduino.h>
+ #include <Arduino.h>
 
-// imports
-#include "MotorDriver.h"
-
-// pinouts
-#define Lside 12
-#define Lsensor 8
-#define Rside 2
-#define Rsensor 4
-#define MSensor 7
-#define StartMod 10
-#define Rpos 6
-#define Rneg 11
-#define Lpos 3
-#define Lneg 5
-#define switch1 A6
-#define switch2 A7
+ // imports
+ #include "sensors/MotorDriver.h"
+ #include "states/robotState.h"
+ #include "action/robotAction.h"
+ #include "states/moveForward.h"
  
-// define objects
-MotorDriver *leftMotorDriver;
-MotorDriver *rightMotorDriver;
-
-// define functions
-void updateMotors();
-
-void setup() {
-    // define pinmodes
-    pinMode(Rpos, OUTPUT);
-    pinMode(Rneg, OUTPUT);
-    pinMode(Lpos, OUTPUT);
-    pinMode(Lneg, OUTPUT);
-    pinMode(StartMod, INPUT);
-    pinMode(switch1, INPUT);
-    pinMode(switch2, INPUT);
-
-    // instantiate objects
-    leftMotorDriver = new MotorDriver();
-    rightMotorDriver = new MotorDriver();
-
-    Serial.begin(9600);
-
-    // wait for start signal
-    while (!digitalRead(StartMod)) {
-      Serial.print(digitalRead(StartMod));
-      Serial.println(" Waiting for start signal");
-    }
-}
-
-void loop() {
-    // pollsensors()
-    // updateState()
-    updateMotors();
-
-    // listen for stop signal
-    if (!digitalRead(StartMod)) {
-      while(true) {
-        // brake()
-        Serial.println("braking");
-      }
-    }
-    // debug()
-    
-}
-
-/**
- * Implemented for Shorti's motordrivers to conform to the
- * simple motordriver with speed and direction.  
- */ 
-void updateMotors() {
-    int leftDirection = leftMotorDriver->getDirection();
-    int leftSpeed = leftMotorDriver->getSpeed();
-
-    if (leftDirection == 1) {  // if direction is forward
-        analogWrite(Lpos, leftSpeed);
-        analogWrite(Lneg, 0);
-    } else {                    // if direction is back
-        analogWrite(Lpos, 0);
-        analogWrite(Lneg, leftSpeed);
-    }
-
-    int rightDirection = rightMotorDriver->getDirection();
-    int rightSpeed = rightMotorDriver->getSpeed();
-
-    if (rightDirection == 1) {  // if direction is forward
-        analogWrite(Rpos, rightSpeed);
-        analogWrite(Rneg, 0);
-    } else {                    // if direction is back
-        analogWrite(Rpos, 0);
-        analogWrite(Rneg, rightSpeed);
-    }
-}
+ // pinouts
+ #define Lside 12
+ #define Lsensor 8
+ #define Rside 2
+ #define Rsensor 4
+ #define MSensor 7
+ #define StartMod 10
+ #define Rpos 6
+ #define Rneg 11
+ #define Lpos 3
+ #define Lneg 5
+ #define switch1 A6
+ #define switch2 A7
+  
+ // define objects
+ MotorDriver *leftMotorDriver;
+ MotorDriver *rightMotorDriver;
+ 
+ // define functions
+ void updateMotors();
+ void stop();
+ MoveForward* rstate;
+ RobotAction* raction;
+ 
+ void setup() {
+     // define pinmodes
+     pinMode(Rpos, OUTPUT);
+     pinMode(Rneg, OUTPUT);
+     pinMode(Lpos, OUTPUT);
+     pinMode(Lneg, OUTPUT);
+     pinMode(StartMod, INPUT);
+     pinMode(switch1, INPUT);
+     pinMode(switch2, INPUT);
+ 
+     // instantiate objects
+     leftMotorDriver = new MotorDriver();
+     rightMotorDriver = new MotorDriver();
+ 
+     Serial.begin(9600);
+ 
+     raction = new RobotAction(leftMotorDriver, rightMotorDriver);
+     rstate = new MoveForward(raction);
+     // wait for start signal
+     while (!digitalRead(StartMod)) {
+       Serial.print(digitalRead(StartMod));
+       Serial.println(" Waiting for start signal");
+     }
+ }
+ void loop() {
+     // pollsensors()
+     // updateState()
+     updateMotors();
+     if (digitalRead(MSensor)) {
+       rstate->runAlgorithm();
+     } else {
+       stop();
+     }
+     // listen for stop signal
+     if (!digitalRead(StartMod)) {
+       while(true) {
+         //brake
+         stop();
+       }
+     }
+     
+ }
+ 
+ void stop() {
+   analogWrite(Rpos, 0);
+   analogWrite(Rneg, 0);
+   analogWrite(Lpos, 0);
+   analogWrite(Lneg, 0);
+ }
+ 
+ /**
+  * Implemented for Shorti's motordrivers to conform to the
+  * simple motordriver with speed and direction.  
+  */ 
+ void updateMotors() {
+     int leftDirection = leftMotorDriver->getDir();
+     int leftSpeed = leftMotorDriver->getSpeed();
+ 
+     if (leftDirection == 1) {  // if direction is forward
+         analogWrite(Lpos, leftSpeed);
+         analogWrite(Lneg, 0);
+     } else {                    // if direction is back
+         analogWrite(Lpos, 0);
+         analogWrite(Lneg, leftSpeed);
+     }
+ 
+     int rightDirection = rightMotorDriver->getDir();
+     int rightSpeed = rightMotorDriver->getSpeed();
+ 
+     if (rightDirection == 1) {  // if direction is forward
+         analogWrite(Rpos, rightSpeed);
+         analogWrite(Rneg, 0);
+     } else {                    // if direction is back
+         analogWrite(Rpos, 0);
+         analogWrite(Rneg, rightSpeed);
+     }
+ }
