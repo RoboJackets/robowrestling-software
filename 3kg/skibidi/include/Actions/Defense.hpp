ddc32b5f50538a716e059422173eeb72d5411423
LawsOfScience <thesandwichsupreme@gmail.com>
2025-03-13 20:47:41 -0400

    Add advanced flinch + inch Defense behavior

--- 3kg/skibidi/include/Actions/Defense.hpp
+++ 3kg/skibidi/include/Actions/Defense.hpp
@@ -3,12 +3,22 @@
 
 #include "Actions/Action.hpp"
 
-#define FLINCH_MILLIS 100
+#define FLINCH_MILLIS     100
+#define UNTIL_INCH_MILLIS 3000
+#define INCH_MILLIS       50
+#define INCH_WAIT_MILLIS  1000
+
+enum Phase {
+  PRE_FLINCH, FLINCHING,
+  PRE_INCH, INCH_START, INCH_FWD, INCH_WAIT,
+  CHARGING
+};
 
 class Defense : public Action {
     private:
-        bool have_flinched;
+        enum Phase phase;
         int flinch_started_millis;
+        int inch_started_millis;
     public:
         Defense();
         virtual void make_decision(State* state);
--- 3kg/skibidi/src/Actions/Defense.cpp
+++ 3kg/skibidi/src/Actions/Defense.cpp
@@ -1,18 +1,58 @@
 #include "Actions/Defense.hpp"
 
 void Defense::make_decision(State *state) {
-    if (!this->have_flinched) {
-        if (this->flinch_started_millis == -1)
+    switch (this->phase) {
+        case PRE_FLINCH:
             this->flinch_started_millis = millis();
+            this->phase = Phase::FLINCHING;
+            // Fall-through is deliberate
+        case FLINCHING:
+            this->phase = (millis() - this->flinch_started_millis)
+                >= FLINCH_MILLIS ? Phase::PRE_INCH : Phase::FLINCHING;
+            state->driving_state = DrivingState::MFORWARD;
+            state->motor_speed = 50;
+            break;
 
-        this->have_flinched =
-            (millis() - this->flinch_started_millis) >= FLINCH_MILLIS;
-        state->driving_state = DrivingState::MFORWARD;
-        state->motor_speed = 50;
+        case PRE_INCH:
+            this->phase = (millis() - (this->flinch_started_millis + FLINCH_MILLIS))
+                >= UNTIL_INCH_MILLIS ? Phase::INCH_FWD : Phase::PRE_INCH;
+            state->motor_speed = 0;
+            break;
+
+        case INCH_START:
+            this->inch_started_millis = millis();
+            this->phase = Phase::INCH_FWD;
+            // Fall-through is deliberate
+        case INCH_FWD:
+            // TODO: adjust to turn towards opponent if opponent moves
+            this->phase = (millis() - this->inch_started_millis)
+                >= INCH_MILLIS ? Phase::INCH_WAIT : Phase::INCH_FWD;
+            state->driving_state = DrivingState::MFORWARD;
+            state->motor_speed = 50;
+            break;
+
+        case INCH_WAIT:
+            this->phase = (millis() - (this->inch_started_millis + INCH_MILLIS))
+                >= INCH_WAIT_MILLIS ? Phase::INCH_START : Phase::INCH_WAIT;
+
+            // NOTE: will have center sensor tuned to detect only
+            // at close range
+            this->phase = state->active_ir_sensors[IrDirection::CENTER]
+                ? Phase::CHARGING : this->phase;
+
+            state->motor_speed = 0;
+
+            break;
+
+        case CHARGING:
+            // TODO: import slammy whammy logic or otherwise
+            // delegate to slammy whammy
+            break;
     }
 }
 
 Defense::Defense() {
-    this->have_flinched = false;
+    this->phase = Phase::PRE_FLINCH;
     this->flinch_started_millis = -1;
+    this->inch_started_millis = -1;
 }
