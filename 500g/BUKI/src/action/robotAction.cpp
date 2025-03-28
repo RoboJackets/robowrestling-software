46f0e79ef15c6cb6dc990a21947fe393229cc4fa
lobitzb564 <lo.crazyred@gmail.com>
2025-03-27 20:56:55 -0400

    some updates idk

--- 500g/BUKI/src/action/robotAction.cpp
+++ 500g/BUKI/src/action/robotAction.cpp
@@ -8,40 +8,40 @@ RobotAction::RobotAction(MotorDriver* leftMotorParameter, MotorDriver* rightMoto
 
 void RobotAction::turnLeft() {
     // call setDir and setSpeed
-    leftMotor->setDir(1);           // leftMotor is a pointer to an object created in main, so use "->" to set direction. If it was a normal object (not a pointer), use leftMotor.setDir(false)
-    rightMotor->setDir(0);
+    leftMotor->setDir(0);           // leftMotor is a pointer to an object created in main, so use "->" to set direction. If it was a normal object (not a pointer), use leftMotor.setDir(false)
+    rightMotor->setDir(1);
     rightMotor->setSpeed(125);
     leftMotor->setSpeed(90);
 }
 
 void RobotAction::turnRight() {
     // call setDir and setSpeed
-    leftMotor->setDir(0);
-    rightMotor->setDir(1);
+    leftMotor->setDir(1);
+    rightMotor->setDir(0);
     leftMotor->setSpeed(125);
     rightMotor->setSpeed(90);
 }
 
 void RobotAction::go() {
     // call setDir and setSpeed
-    leftMotor->setDir(1);
-    rightMotor->setDir(1);
+    leftMotor->setDir(0);
+    rightMotor->setDir(0);
     leftMotor->setSpeed(125);
     rightMotor->setSpeed(125);
 }
 
 void RobotAction::GOGOGO() {
     // call setDir and setSpeed
-    leftMotor->setDir(0);
-    rightMotor->setDir(0);
+    leftMotor->setDir(1);
+    rightMotor->setDir(1);
     leftMotor->setSpeed(255);
     rightMotor->setSpeed(255);
 }
 
 void RobotAction::reverse() {
     // call setDir and setSpeed
-    leftMotor->setDir(1);
-    rightMotor->setDir(1);
+    leftMotor->setDir(0);
+    rightMotor->setDir(0);
     leftMotor->setSpeed(125);
     rightMotor->setSpeed(125);
 }
--- 500g/BUKI/src/main.cpp
+++ 500g/BUKI/src/main.cpp
@@ -1,106 +1,101 @@
 /**
  * Joe
- * V1.1
  * File that outlines Shorti's main
  * 1/30/2025
  */
 
  #include <Arduino.h>
- 
-// imports
-#include "sensors/MotorDriver.h"
-#include "states/robotState.h"
-#include "action/robotAction.h"
-#include "sensors/LINEsensor.h"
-#include "sensors/IRsensor.h"
-#include "states/stayOn.h"
+
+ // imports
+ #include "sensors/MotorDriver.h"
+ #include "states/robotState.h"
+ #include "action/robotAction.h"
+ #include "sensors/LINEsensor.h"
+ #include "sensors/IRsensor.h"
+ #include "states/stayOn.h"
  
  // pinouts
- const int LEFT_IR_90 = 12;
- const int LEFT_IR_45 = 8;
- const int RIGHT_IR_90 = 2;
- const int RIGHT_IR_45 = 4;
- const int MIDDLE_IR = 7;
- const int START_MODULE = 10;
- const int R_POS = 11;
- const int R_NEG = 13;
- const int L_POS = 6;
- const int L_NEG = A5;
- const int R_PWM = 5;
- const int L_PWM = 3;
- const int DIP_SWITCH_1 = A6;
- const int DIP_SWITCH_2 = A7;
- const int LEFT_LINE = A0;
- const int RIGHT_LINE = A1;
- 
+ #define Lside 12
+ #define Lsensor 8
+ #define Rside 2
+ #define Rsensor 4
+ #define MSensor 7
+ #define StartMod 10
+ #define Rpos 6
+ #define Rneg 11
+ #define Lpos 3
+ #define Lneg 5
+ #define switch1 A6
+ #define switch2 A7
+  
  // define objects
  MotorDriver *leftMotorDriver;
  MotorDriver *rightMotorDriver;
  
-// define functions
-void updateMotors();
-void stop();
-void pollsensors();
-
-
-StayOn* rstate;
-RobotAction* raction;
-WorldState* wrldstate;
-LINEsensor* linesensors [3];
- 
+ // define functions
+ void updateMotors();
+ void stop();
+ RobotAction* raction;
+ StayOn* rstate;
+  WorldState* wrldstate;
+  LINEsensor* linesensors [3];
  void setup() {
      // define pinmodes
-     pinMode(R_POS, OUTPUT);
-     pinMode(R_NEG, OUTPUT);
-     pinMode(L_POS, OUTPUT);
-     pinMode(L_NEG, OUTPUT);
-     pinMode(LEFT_IR_90, INPUT);
-     pinMode(LEFT_IR_45, INPUT);
-     pinMode(MIDDLE_IR, INPUT);
-     pinMode(RIGHT_IR_45, INPUT);
-     pinMode(RIGHT_IR_90, INPUT);
-     pinMode(LEFT_LINE, INPUT);
-     pinMode(RIGHT_LINE, INPUT);
-     pinMode(START_MODULE, INPUT);
-     pinMode(DIP_SWITCH_1, INPUT);
-     pinMode(DIP_SWITCH_2, INPUT);
+     pinMode(Rpos, OUTPUT);
+     pinMode(Rneg, OUTPUT);
+     pinMode(Lpos, OUTPUT);
+     pinMode(Lneg, OUTPUT);
+     pinMode(StartMod, INPUT);
+     pinMode(switch1, INPUT);
+     pinMode(switch2, INPUT);
+ 
+     // instantiate objects
      leftMotorDriver = new MotorDriver();
      rightMotorDriver = new MotorDriver();
-   
+ 
      Serial.begin(9600);
-   
+ 
      for (int i = 0; i < 3; i++) {
-       linesensors[i] = new LINEsensor(0);
+        linesensors[i] = new LINEsensor(0);
+      }
+      raction = new RobotAction(leftMotorDriver, rightMotorDriver);
+      wrldstate = new WorldState(linesensors, NULL);
+      rstate = new StayOn(raction, wrldstate);
+     // wait for start signal
+     while (!digitalRead(StartMod)) {
+       Serial.print(digitalRead(StartMod));
+       Serial.println(" Waiting for start signal");
      }
-     raction = new RobotAction(leftMotorDriver, rightMotorDriver);
-     wrldstate = new WorldState(linesensors, NULL);
-     rstate = new StayOn(raction, wrldstate);
-   }
-   void loop() {
-       pollsensors();
-       // updateState()
-       updateMotors();
-       
+ }
+ void pollsensors() {
+  linesensors[0]->setValue(analogRead(A0));
+  linesensors[1]->setValue(analogRead(A1));
+}
+ void loop() {
+      pollsensors();
+     // updateState()
+     updateMotors();
+     if (digitalRead(MSensor)) {
        rstate->runAlgorithm();
-
-       // listen for stop signal
-       // if (!digitalRead(START_MODULE)) {
-       //   while(true) {
-       //     //brake
-       //     stop();
-       //   }
-       // }
-       
-   }
-   void stop() {
-     analogWrite(L_PWM, 0);
-     analogWrite(R_PWM, 0);
-   }
-   
-   void pollsensors() {
-     linesensors[0]->setValue(analogRead(LEFT_LINE));
-     linesensors[1]->setValue(analogRead(RIGHT_LINE));
-   }
+     } else {
+       stop();
+     }
+     // listen for stop signal
+     if (!digitalRead(StartMod)) {
+       while(true) {
+         //brake
+         stop();
+       }
+     }
+     
+ }
+ 
+ void stop() {
+   analogWrite(Rpos, 0);
+   analogWrite(Rneg, 0);
+   analogWrite(Lpos, 0);
+   analogWrite(Lneg, 0);
+ }
  
  /**
   * Implemented for Shorti's motordrivers to conform to the
@@ -111,25 +106,21 @@ LINEsensor* linesensors [3];
      int leftSpeed = leftMotorDriver->getSpeed();
  
      if (leftDirection == 1) {  // if direction is forward
-        analogWrite(L_POS, 1);
-        analogWrite(L_NEG, 0);
+         analogWrite(Lpos, leftSpeed);
+         analogWrite(Lneg, 0);
      } else {                    // if direction is back
-        analogWrite(L_POS, 0);
-        analogWrite(L_NEG, 1);
+         analogWrite(Lpos, 0);
+         analogWrite(Lneg, leftSpeed);
      }
  
      int rightDirection = rightMotorDriver->getDir();
      int rightSpeed = rightMotorDriver->getSpeed();
  
      if (rightDirection == 1) {  // if direction is forward
-        analogWrite(R_POS, 1);
-        analogWrite(R_NEG, 0);
+        analogWrite(Rpos, rightSpeed);
+        analogWrite(Rneg, 0);
      } else {                    // if direction is back
-        analogWrite(R_POS, 0);
-        analogWrite(R_NEG, 1);
+        analogWrite(Rpos, 0);
+        analogWrite(Rneg, rightSpeed);
      }
-
-     // controls the speed
-     analogWrite(L_PWM, leftSpeed);
-     analogWrite(R_PWM, rightSpeed);
  }
