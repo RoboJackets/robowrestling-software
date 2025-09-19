e3f51d12cca71ff75171918c9a201dc785ffc610
Nathan Zheng <NathanZheng50@gmail.com>
2025-09-18 20:44:46 -0400

    Added Algorithms base code. Need to work on RobotActions

---
+++ 3kg/Kitti/include/Robot/Algorithms.hpp
@@ -0,0 +1,22 @@
+#include "Robot/Robot_Actions.hpp"
+#include "Robot/World_State.hpp"
+#include "Enums/enemy_states.hpp"
+#include "Enums/line_states.hpp"
+
+#ifndef ALGORITHMS_HPP
+#define ALGORITHMS_HPP
+
+class Algorithms {
+    private:
+        RobotActions* actions;
+        WorldState* world_state;
+        line_states self_position;
+        enemy_states enemy_position;
+    public:
+        Algorithms(line_states self_pos, enemy_states enemy_pos);
+        void match_strategy();
+        void slammy_whammy();
+        void attack_forward();
+}
+
+#endif // ALGORITHMS_HPP
